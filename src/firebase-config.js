9b84c13463e9373133fb22f5624da432c7e98ee5
Ozkhan Mamedov <ozhanm96@yandex.ru>
2022-12-03 12:34:49 +0400

    added firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const apiKey = process.env.REACT_APP_API_KEY;
+const firebaseConfig = {
+  apiKey,
+  authDomain: "woman-up-backend.firebaseapp.com",
+  projectId: "woman-up-backend",
+  storageBucket: "woman-up-backend.appspot.com",
+  messagingSenderId: "950343160293",
+  appId: "1:950343160293:web:d9b75b90f4ccda0fc0df0c",
+  measurementId: "G-F3YC3PDS7S"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
